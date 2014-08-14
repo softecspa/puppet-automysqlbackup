756b32ebbcf5e968e7e8f2052e0b2427c39219d3
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-08-14 13:46:28 +0000

    fix

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,12 @@
 class automysqlbackup::install {
 
   if $::lsbdistcodename == 'hardy' {
-    apt::ppa { 'jeff-bitprophet/ppa':
-      key     => '3FCA1EF2',
-      mirror  => true
+    softec_apt::ppa {'jeff-bitprophet/ppa':
+      mirror  => true,
+      key     => '3FCA1EF2'
     }
 
-    Apt::Ppa['jeff-bitprophet/ppa'] ->
+    Softec_apt::Ppa['jeff-bitprophet/ppa'] ->
     Package['automysqlbackup']
   }
 
